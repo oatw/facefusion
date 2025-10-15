67e2ab07e61790821e24884b00b6ade6bccf3062
oatw <oatwoatw@icloud.com>
2025-10-15 10:14:12 +0800

    Disable nsfw

--- facefusion/core.py
+++ facefusion/core.py
@@ -116,21 +116,22 @@ def pre_check() -> bool:
 
 
 def common_pre_check() -> bool:
-	common_modules =\
-	[
-		content_analyser,
-		face_classifier,
-		face_detector,
-		face_landmarker,
-		face_masker,
-		face_recognizer,
-		voice_extractor
-	]
+	return True
+	# common_modules =\
+	# [
+	# 	content_analyser,
+	# 	face_classifier,
+	# 	face_detector,
+	# 	face_landmarker,
+	# 	face_masker,
+	# 	face_recognizer,
+	# 	voice_extractor
+	# ]
 
-	content_analyser_content = inspect.getsource(content_analyser).encode()
-	is_valid = hash_helper.create_hash(content_analyser_content) == 'b159fd9d'
+	# content_analyser_content = inspect.getsource(content_analyser).encode()
+	# is_valid = hash_helper.create_hash(content_analyser_content) == 'b159fd9d'
 
-	return all(module.pre_check() for module in common_modules) and is_valid
+	# return all(module.pre_check() for module in common_modules) and is_valid
 
 
 def processors_pre_check() -> bool:
